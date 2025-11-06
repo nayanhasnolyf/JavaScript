7735e74bd368673434fb0cb106787784060f0809
Nayan Hasnolyf <nayanpokhriyal25@gmail.com>
2025-11-07 00:13:37 +0530

    new Commit

---
+++ 01_v.js
@@ -0,0 +1,5 @@
+const accountId = 144333
+let accountEmail = "nayanpokhriyal25@gmail.com"
+var accountpassword = "12345"
+accountcity = "New Delhi"
+console.table([accountId, accountEmail, accountpassword, accountcity])
---
+++ 02_datatypes.js
@@ -0,0 +1,9 @@
+let age = 18
+let isLoggedIn = false
+let state = null
+let city
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+console.log(typeof state)
+console.log(typeof city)
+
